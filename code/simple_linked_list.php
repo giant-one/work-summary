3d8fd712b519c53f76be5fb790ce3d8052ee22b5
xuce <xucece2014@163.com>
2021-03-02 23:08:32 +0800

    '练习题'

--- code/simple_linked_list.php
+++ code/simple_linked_list.php
@@ -1,4 +1,18 @@
 <?php
+
+/*
+ * 练习题
+ * 1） 求单链表中有效节点个数
+ * 2） 查找单链表中倒数第k个节点【新浪面试题】
+ * 3） 单链表的反转【腾讯面试题】
+ * 4） 从头到尾打印单链表【百度，要求方式1: 反转遍历。方式2: Stack栈】
+ * 5） 合并两个有序的单链表，合并之后的链表依然有序
+ */
+
+/*
+ * 求单链表中有效节点个数
+ * 其实就是链表的遍历
+ */
 /*
 $root_node = new Node(0, '', '');
 $simple_linked_list = new SimpleLinkedList($root_node);
@@ -11,6 +25,38 @@ $simple_linked_list->add($node3);
 $simple_linked_list->display();
 */
 
+/*
+ * 查找单链表中倒数第k个节点【新浪面试题】
+ * 1、获取链表长度
+ * 2、遍历链表找到第(length - k)个即可
+ */
+
+$root_node = new Node(0, '', '');
+$simple_linked_list = new SimpleLinkedList($root_node);
+$node1 = new Node('1', '宋江', '及时雨');
+$node2 = new Node('2', '吴用', '智多星');
+$node3 = new Node('3', '李逵', '小旋风');
+$simple_linked_list->add($node1);
+$simple_linked_list->add($node2);
+$simple_linked_list->add($node3);
+var_dump($simple_linked_list->getNodeByBackIndex(0));
+
+
+
+/*
+顺序添加
+$root_node = new Node(0, '', '');
+$simple_linked_list = new SimpleLinkedList($root_node);
+$node1 = new Node('1', '宋江', '及时雨');
+$node2 = new Node('2', '吴用', '智多星');
+$node3 = new Node('3', '李逵', '小旋风');
+$simple_linked_list->add($node1);
+$simple_linked_list->add($node2);
+$simple_linked_list->add($node3);
+$simple_linked_list->display();
+*/
+
+/*
 $root_node = new Node(0, '', '');
 $simple_linked_list = new SimpleLinkedList($root_node);
 $node1 = new Node('1', '宋江', '及时雨');
@@ -22,7 +68,7 @@ $simple_linked_list->addOrderByNo($node4);
 $simple_linked_list->addOrderByNo($node3);
 $simple_linked_list->addOrderByNo($node2);
 $simple_linked_list->display();
-
+*/
 
 Class SimpleLinkedList
 {
@@ -78,6 +124,41 @@ Class SimpleLinkedList
 
             $temp = $temp->next;
         }
+    }
+
+    public function getLength()
+    {
+        $length = 0;
+        if ($this->root_node->next == null) {
+            return $length;
+        }
+
+        $cur = $this->root_node->next;
+        while ($cur != null) {
+            $length++;
+            $cur = $cur->next;
+        }
+
+        return $length;
+    }
+
+    public function getNodeByBackIndex($index)
+    {
+        if ($this->root_node->next == null) {
+            exit('链表为空');
+        }
+
+        $length = $this->getLength();
+
+        if ($index < 0 && $index > $length) {
+            exit('index error');
+        }
+
+        $cur = $this->root_node->next;
+        for ($i = 0; $i < $length - $index; $i++) {
+            $cur = $cur->next;
+        }
+        return $cur;
     }
 }
 
